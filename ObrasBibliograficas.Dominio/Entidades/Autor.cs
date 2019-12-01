16605dd36aa3b9f863a178cc2a1033771ce5e9bd
gau_figueiredo@hotmail.com <gau_figueiredo@hotmail.com>
2019-11-30 22:33:48 -0300

    Add project files.

---
+++ ObrasBibliograficas.Dominio/Entidades/Autor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObrasBibliograficas.Dominio.Entidades
+{
+    public class Autor: EntidadeBase
+    {
+        public string Nome { get; set; }
+    }
+}
---
+++ ObrasBibliograficas.Dominio/Interfaces/Servicos/IAutorServico.cs
@@ -0,0 +1,12 @@
+﻿using ObrasBibliograficas.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObrasBibliograficas.Dominio.Interfaces.Servicos
+{
+    public interface IAutorServico : IServicoBase<Autor>
+    {
+
+    }
+}
